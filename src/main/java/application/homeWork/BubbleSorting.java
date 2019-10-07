79b4fed979ab3787d7d9a579b39297675e451104
andr3wl0ng <andriy.dovgiy@gmail.com>
2019-10-07 19:54:29 +0300

    bubble sort

---
+++ src/main/java/application/homeWork/BubbleSorting.java
@@ -0,0 +1,30 @@
+package application.homeWork;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class BubbleSorting {
+    public static void main(String[] args) {
+        int[] arr = new int[10];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = new Random().nextInt() % 50;
+            System.out.println(arr[i]);
+        }
+        for (int i = 0; i < arr.length; ++i) {
+            for (int j = 0; j < arr.length-1; ++j) {
+
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+//                    arr[j] = arr[j + 1] + arr[j];
+//                    arr[j + 1] = arr[j] - arr[j + 1];
+//                    arr[j] = arr[j] - arr[j+1];
+                }
+                System.out.println("-------\n");
+                System.out.println(Arrays.toString(arr));
+            }
+        }
+    }
+}
