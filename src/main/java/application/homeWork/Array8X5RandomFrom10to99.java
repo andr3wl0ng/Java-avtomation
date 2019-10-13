cab69f2baa3ca9510bf13e1a5f5b2031e49e7a2a
andr3wl0ng <andriy.dovgiy@gmail.com>
2019-10-13 22:12:19 +0300

    2DIM Araay from 10 to 99

---
+++ src/main/java/application/homeWork/Array8X5RandomFrom10to99.java
@@ -0,0 +1,20 @@
+package application.homeWork;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Random;
+
+public class Array8X5RandomFrom10to99 {
+    public static void main(String[] args) {
+        int [][] doubleArray = new int[8][5];
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 5; j++) {
+                //doubleArray[i][j] = new Random().nextInt();
+                doubleArray[i][j] = (int) (Math.random()  * 90)+10;
+                System.out.print(doubleArray[i][j]+ "  ");
+            }
+          System.out.println( );
+        }
+
+    }
+}
