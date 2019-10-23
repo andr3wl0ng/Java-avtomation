52e7eb01174cb19d76f953c7edbd74c6cf1c062a
andriy.dovgiy@paazl.com <andriy.dovgiy@paazl.com>
2019-10-23 13:57:28 +0200

    coffe machine is ready

--- src/main/java/application/coffeeMachine/CoffeeMachine.java
+++ src/main/java/application/coffeeMachine/CoffeeMachine.java
@@ -4,15 +4,45 @@ import java.util.Scanner;
 
 public class CoffeeMachine {
     public static void main(String[] args) {
-        System.out.println("Select your drink :");
+        SelectYourDrink();
+
+
+        boolean statusOK = true;
+        while (statusOK){
+            Scanner scanner = new Scanner(System.in);
+            int selection = scanner.nextInt();
+
+            Recipes.Drink drink = Recipes.Drink.getDrinkByType(selection);
+            if (drink == null){
+                System.out.println("Wrong Drink Type");
+            } else {
+                statusOK = CoffeeMaker.prepareDrink(drink);
+                System.out.println(CoffeeMaker.coffeeTankCapacity);
+                System.out.println(CoffeeMaker.waterTankCapacity);
+                System.out.println(CoffeeMaker.milkTankCapacity);
+                System.out.println(CoffeeMaker.trashTankCapacity);
+                System.out.println("__________________________");
+                System.out.println("You drink: "+ drink + " Is ready. Please, enjoy");
+
+                System.out.println("__________________________");
+                SelectYourDrink();
+
+            }
+        }
+
+
+
+
+    }
+
+    private static void SelectYourDrink() {
         System.out.println("For 'Espresso' press - 1" );
         System.out.println("For 'Americano' press - 2" );
         System.out.println("For 'Americano With Milk' press - 3" );
-        System.out.println("For 'Double Espresso' press - 4" );
-
-        Scanner scanner = new Scanner(System.in);
-        int Selection = scanner.nextInt();
-
+        System.out.println("For 'Late' press - 4" );
+        System.out.println("For 'Hot Water' press - 5" );
+        System.out.println("__________________________");
+        System.out.println("Select your drink and press ENTER:");
     }
 
 }
--- src/main/java/application/coffeeMachine/Recipes.java
+++ src/main/java/application/coffeeMachine/Recipes.java
@@ -2,29 +2,95 @@ package application.coffeeMachine;
 
 public class Recipes {
 
-    public static void main(String[] args) {
+    /**
+     * index 0 = coffee
+     * index 1 = water
+     * index 2 = milk
+     * index 3 = sugar
+     */
 
 
-        RecipeComponentBase espresso = new RecipeComponentBase();
-        espresso.preparedDrink = espresso.coffee + espresso.water / 2;
+    enum Drink {
 
-        RecipeComponentBase americano = new RecipeComponentBase();
-        americano.preparedDrink = (americano.coffee * 2) + (americano.water * 3);
 
-        RecipeComponentBase americanoWithMilk = new RecipeComponentBase();
-        americanoWithMilk.preparedDrink = (americanoWithMilk.coffee * 2) + (americanoWithMilk.water * 3) + americanoWithMilk.milk;
+        ESPRESSO            (1, 1, 0, 0,1),
+        AMERICANO           (2, 3, 0, 0,2),
+        AMERICANOWITHMILK   (2, 3, 3, 0,3),
+        LATE                (1, 1, 3, 1,4),
+        HOTWATER            (0, 1, 0, 0,5);
 
-        RecipeComponentBase doubleEspresso = new RecipeComponentBase();
-        doubleEspresso.preparedDrink = espresso.coffee * 2;
+        private final int coffee;
+        private final int water;
+        private final int milk;
+        private final int sugar;
+        private final int drinkType;
 
+        Drink(int coffee, int water, int milk, int sugar, int drinkType) {
+            this.coffee = coffee;
+            this.water = water;
+            this.milk = milk;
+            this.sugar = sugar;
+            this.drinkType = drinkType;
+        }
+
+        public static Drink getDrinkByType(int type) {
+            for (Drink drink : values()) {
+                if (drink.getDrinkType() == type) {
+                    return drink;
+                }
+            }
+            return null;
+        }
+
+
+        public int getCoffee() {
+            return coffee;
+        }
+
+        public int getWater() {
+            return water;
+        }
+
+        public int getMilk() {
+            return milk;
+        }
+
+        public int getSugar() {
+            return sugar;
+        }
+
+        public int getDrinkType() {
+            return drinkType;
+        }
     }
 
-}
 
-class RecipeComponentBase {
-    int coffee = 10;
-    int milk = 10;
-    int water = 10;
 
-    int preparedDrink;
+
+
+//   public static int [] espresso             = new int []{1, 1, 0, 0 };
+//   public static int [] americano            = new int []{2, 3, 0, 0 };
+//   public static int [] americanoWithMilk    = new int []{2, 3, 3, 0 };
+//   public static int [] late                 = new int []{1, 1, 3, 1 };
+//   public static int [] hotWater             = new int []{0, 1, 0, 0 };
+
+
+
+
+
+//    public static void main(String[] args) {
+//
+//
+//        RecipeComponentBase espresso = new RecipeComponentBase();
+//        espresso.preparedDrink = espresso.coffee + espresso.water / 2;
+//
+//        RecipeComponentBase americano = new RecipeComponentBase();
+//        americano.preparedDrink = (americano.coffee * 2) + (americano.water * 3);
+//
+//        RecipeComponentBase americanoWithMilk = new RecipeComponentBase();
+//        americanoWithMilk.preparedDrink = (americanoWithMilk.coffee * 2) + (americanoWithMilk.water * 3) + americanoWithMilk.milk;
+//
+//        RecipeComponentBase doubleEspresso = new RecipeComponentBase();
+//        doubleEspresso.preparedDrink = espresso.coffee * 2;
+
 }
