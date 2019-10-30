36e0042b0c00ab7217682c4b5f33654046bd9eae
andr3wl0ng <andriy.dovgiy@gmail.com>
2019-10-30 16:55:02 +0200

    oop start

---
+++ src/main/java/application/OOP/Main.java
@@ -0,0 +1,11 @@
+package application.OOP;
+
+public class Main {
+    public static void main(String[] args) {
+        OOP oop = new OOP();
+        OOP oop2 = new OOP("testOne");
+        OOP oop3 = new OOP("Olga", "Tamara");
+
+
+    }
+}
---
+++ src/main/java/application/OOP/OOP.java
@@ -0,0 +1,21 @@
+package application.OOP;
+
+public class OOP {
+    public String public_string;
+    private String private_string;
+    protected String protected_string;
+    String default_string;
+
+    OOP(){
+        public_string = "temp";
+    }
+
+    OOP(String newNameOnStart){
+        this.public_string = newNameOnStart;
+    }
+
+    OOP(String s, String ss){
+        public_string = s;
+        System.out.println(ss);
+    }
+}
